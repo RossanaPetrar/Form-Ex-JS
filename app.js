219eb6276d6a1ede7b60621f5848474bdbb6025b
RossanaPetrar <rossana.petrar@gmail.com>
2022-08-02 22:28:54 +0300

    Added Judet and Localitate Link Logic

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const judetOption = document.querySelectorAll(".judet");
+const judetOption = document.querySelector(".judet-container");
 const localitateContainer = document.querySelector("#localitate");
-judetOption.addEventListener("focus", getLocalitati());
 
-function getLocalitati() {
-  console.log(judetOption.textContent);
-  if ((judetOption.value = "Cluj")) {
+function onChange() {
+  let text = judetOption.options[judetOption.selectedIndex].text;
+
+  if (text === "Cluj") {
     localitateContainer.innerHTML = `
-    <option selected>-</option>
-    <option value="Cluj-Napoca" class="localitate">Cluj-Napoca</option>
-    <option value="Turda" class="localitate">Turda</option>
-    <option value="Huedin" class="localitate">Huedin</option>
-    `;
-  } else if ((judetOption.value = "Bihor")) {
+        <option selected>-</option>
+        <option value="Cluj-Napoca" class="localitate">Cluj-Napoca</option>
+        <option value="Turda" class="localitate">Turda</option>
+        <option value="Huedin" class="localitate">Huedin</option>
+        `;
+  } else if (text === "Bihor") {
     localitateContainer.innerHTML = `
-    <option selected>-</option>
-    <option value="Oradea" class="localitate">Oradea</option>
-    <option value="Alesd" class="localitate">Alesd</option>
-    <option value="Beius" class="localitate">Beius</option>
-    `;
+        <option selected>-</option>
+        <option value="Oradea" class="localitate">Oradea</option>
+        <option value="Alesd" class="localitate">Alesd</option>
+        <option value="Beius" class="localitate">Beius</option>
+        `;
   }
 }
+judetOption.onchange = onChange;
+onChange();
