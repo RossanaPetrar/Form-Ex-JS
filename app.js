452ee363371bcfadb2ec15dc97e3f1e8ede4bbed
RossanaPetrar <rossana.petrar@gmail.com>
2022-08-02 23:52:20 +0300

    Gathered input values in JASON format

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Judet and Localitate inputs linked
+
 const judetOption = document.querySelector(".judet-container");
 const localitateContainer = document.querySelector("#localitate");
 
@@ -7,18 +9,33 @@ function onChange() {
   if (text === "Cluj") {
     localitateContainer.innerHTML = `
         <option selected>-</option>
-        <option value="Cluj-Napoca" class="localitate">Cluj-Napoca</option>
-        <option value="Turda" class="localitate">Turda</option>
-        <option value="Huedin" class="localitate">Huedin</option>
+        <option value="Cluj-Napoca" class="localitate" name="localitate">Cluj-Napoca</option>
+        <option value="Turda" class="localitate" name="localitate">Turda</option>
+        <option value="Huedin" class="localitate" name="localitate">Huedin</option>
         `;
   } else if (text === "Bihor") {
     localitateContainer.innerHTML = `
         <option selected>-</option>
-        <option value="Oradea" class="localitate">Oradea</option>
-        <option value="Alesd" class="localitate">Alesd</option>
-        <option value="Beius" class="localitate">Beius</option>
+        <option value="Oradea" class="localitate" name="localitate">Oradea</option>
+        <option value="Alesd" class="localitate" name="localitate">Alesd</option>
+        <option value="Beius" class="localitate" name="localitate">Beius</option>
         `;
   }
 }
 judetOption.onchange = onChange;
 onChange();
+
+// Getting form values as JSON
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const value = Object.fromEntries(data.entries());
+
+  console.log(JSON.stringify(value, null, 2));
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
